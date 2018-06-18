6205df0a45acef8bd157950f10e06bacbdd5c751
Daniel Moesgaard Andersen <moesgaarda@gmail.com>
2018-06-18 13:05:49 +0200

    Major changes to design

--- spa/src/app/app.module.ts
+++ spa/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ValueComponent
+    ValueComponent,
+    LoginComponent
 ],
   imports: [
     BrowserModule,
