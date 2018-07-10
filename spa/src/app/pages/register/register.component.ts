baa7f2f6358fe44c57ebc06962cde8475fe593be
Andreas Stenshøj <and8700and@gmail.com>
2018-07-10 13:02:16 +0200

    register page

---
+++ spa/src/app/pages/register/register.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'ngx-app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent implements OnInit {
+  model: any = {};
+  @Output() cancelRegister = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
