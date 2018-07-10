2c10fa1671ac2a34c184db1e017e287756fe9e35
Andreas Stenshøj <and8700and@gmail.com>
2018-07-10 14:05:05 +0200

    angular jwt token stuff

--- spa/src/app/_services/auth.service.ts
+++ spa/src/app/_services/auth.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '../../../node_modules/@angular/http';
+import { tokenNotExpired, JwtHelper } from 'angular2-jwt';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthService {
     baseUrl = 'http://localhost:5000/api/auth/';
     userToken: any;
+    decodedToken: any;
+    jwtHelper: JwtHelper = new JwtHelper();
 
     constructor(private http: Http) { }
 
     login(model: any) {
 
-        return this.http.post(this.baseUrl + 'login', model, this.requestOptions()).map((response: Response) => {
+        return this.http.post(this.baseUrl + 'login', model, this.requestOptions())
+        .map((response: Response) => {
             const user = response.json();
-            if (user) {
+            if (user && user.tokenString) {
                 localStorage.setItem('token', user.tokenString);
+                this.decodedToken = this.jwtHelper.decodeToken(user.tokenString);
+                console.log(this.decodedToken);
                 this.userToken = user.tokenString;
             }
         });
@@ -22,6 +28,10 @@ export class AuthService {
 
     register(model: any) {
         return this.http.post(this.baseUrl + 'register', model, this.requestOptions());
+    }
+
+    loggedIn() {
+        return tokenNotExpired('token');
     }
 
     private requestOptions() {
