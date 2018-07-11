f6e1905d34e2d40bae5c005c6a93f1e25eddb481
Unknown <rasmusedu@gmail.com>
2018-07-11 11:03:20 +0200

    deleted smart table folder

--- spa/src/app/pages/tables/tables-routing.module.ts
+++ spa/src/app/pages/tables/tables-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TablesComponent } from './tables.component';
-import { SmartTableComponent } from './smart-table/smart-table.component';
 import { EmployeeTableComponent } from './employee-table/employee-table.component';
 import { ItemTableComponent } from './item-table/item-table.component';
 import { ItemTemplateTableComponent } from './item-template-table/item-template-table.component';
@@ -13,9 +12,6 @@ const routes: Routes = [{
   path: '',
   component: TablesComponent,
   children: [{
-    path: 'smart-table',
-    component: SmartTableComponent,
-  }, {
     path: 'employee-table',
     component: EmployeeTableComponent,
   }, {
@@ -42,7 +38,6 @@ export class TablesRoutingModule { }
 
 export const routedComponents = [
   TablesComponent,
-  SmartTableComponent,
   EmployeeTableComponent,
   ItemTableComponent,
   ItemTemplateTableComponent,
