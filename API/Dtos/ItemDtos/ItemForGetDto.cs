26dd2a98019fce6cc683e0dfed5683fbcb0f61d7
Daniel Moesgaard <moesgaarda@gmail.com>
2018-07-27 10:39:56 +0200

    Included order in item/get

--- API/Dtos/ItemDtos/ItemForGetDto.cs
+++ API/Dtos/ItemDtos/ItemForGetDto.cs
@@ -11,7 +11,7 @@ namespace API.Dtos
         public string Placement { get; set; }
         public int Amount { get; set; }
         public ItemTemplateForTableDto Template { get; set; }
-        public Order Order { get; set; }
+        public OrderForGetDto Order { get; set; }
         public UserForItemGetDto CreatedBy { get; set; } 
         public ICollection<ItemItemRelationForGet> Parts { get; set; }
         public ICollection<ItemItemRelationForGet> PartOf { get; set; }
