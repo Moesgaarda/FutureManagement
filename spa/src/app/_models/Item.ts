6d7046c6858c75f2abc9569f0a84e3783a7aa612
Daniel Moesgaard <moesgaarda@gmail.com>
2018-09-22 15:13:24 +0200

    TM-2 : Updated models.

--- spa/src/app/_models/Item.ts
+++ spa/src/app/_models/Item.ts
@@ -15,3 +15,4 @@ export interface Item {
     parts: ItemItemRelation[];
     properties: ItemPropertyDescription[];
 }
+
---
+++ spa/src/app/_models/User.ts
@@ -0,0 +1,13 @@
+import { UserRole } from './UserRole';
+
+export interface User {
+    id: number;
+    username: string;
+    role: UserRole;
+    name: string;
+    surname: string;
+    birthdate: Date;
+    isActive: boolean;
+    email: string;
+    phone: string;
+}
