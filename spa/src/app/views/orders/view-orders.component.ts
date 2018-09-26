c98f1ed5893dae5e380d1c715edd4bdb4c3115b4
Daniel Moesgaard <moesgaarda@gmail.com>
2018-09-26 12:39:38 +0200

    TM-76 : orders components added

---
+++ spa/src/app/views/orders/view-orders.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment';
+
+@Component({
+  templateUrl: './view-orders.component.html'
+})
+
+export class ViewOrdersComponent {
+}
