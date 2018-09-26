51443f192d8cea3b0f20169bb2d42c25d83cc774
Daniel Moesgaard <moesgaarda@gmail.com>
2018-09-26 20:50:55 +0200

    TM-76 : Added comps

--- spa/src/app/views/projects/projects.module.ts
+++ spa/src/app/views/projects/projects.module.ts
@@ -7,10 +7,19 @@ import { DetailsProjectComponent } from './details-project.component';
 
 import { ProjectsRoutingModule } from './projects-routing.module';
 
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgSelectModule } from '@ng-select/ng-select';
+
+
 @NgModule({
   imports: [
     ProjectsRoutingModule,
     Ng2SmartTableModule,
+    Ng2SmartTableModule,
+    FormsModule,
+    CommonModule,
+    NgSelectModule
   ],
   declarations: [
     ViewProjectsComponent,
