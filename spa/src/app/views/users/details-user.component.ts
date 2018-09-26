ac2cfd345c428595ad6ef475de6b7722d722957c
Frederik Schrøder <frederikhtxq12@gmail.com>
2018-09-26 21:55:18 +0200

    some user stuff added

--- spa/src/app/views/users/details-user.component.ts
+++ spa/src/app/views/users/details-user.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
-import { environment } from '../../../environments/environment';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../_models/User';
+import { UserService } from '../../_services/user.service';
+import { ActivatedRoute } from '../../../../node_modules/@angular/router';
 
 @Component({
   templateUrl: './details-user.component.html'
 })
+export class DetailsUserComponent implements OnInit {
+  nameDisabled: boolean;
+  userNameDisabled: boolean;
+  surnameDisabled: boolean;
+  emailDisabled: boolean;
+  user: User;
 
-export class DetailsUserComponent {
+  constructor(private userService: UserService, private route: ActivatedRoute) {
+   }
+
+  ngOnInit() {
+    this.nameDisabled = true;
+    this.loadUserOnInit();
+  }
+
+  loadUserOnInit() {
+    this.userService.getUser(+this.route.snapshot.params['id'])
+      .subscribe(user => {
+        this.user = user;
+      });
+  }
+
+  enableName() {
+    if (this.nameDisabled) {
+      this.nameDisabled = false;
+    } else {
+      // indsæt i db
+      this.nameDisabled = true;
+    }
+  }
 }
