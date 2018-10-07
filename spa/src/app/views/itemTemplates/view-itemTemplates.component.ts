3051a4f533d9daf86fe17d690e0d1e4ca7f44bbc
Daniel Moesgaard Andersen <moesgaarda@gmail.com>
2018-10-07 16:05:42 +0200

    FM-96 - Pages now have pagination that works

--- spa/src/app/views/itemTemplates/view-itemTemplates.component.ts
+++ spa/src/app/views/itemTemplates/view-itemTemplates.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation  } from '@angular/core';
 
 import { ItemTemplate, UnitType } from '../../_models/ItemTemplate';
 import { ItemTemplateService } from '../../_services/itemTemplate.service';
@@ -7,34 +7,36 @@ import * as _ from 'underscore';
 
 @Component({
   templateUrl: './view-itemTemplates.component.html',
-  styles: ['.pager li.btn:active { box-shadow: none; }'],
   encapsulation: ViewEncapsulation.None
 })
 
 export class ViewItemTemplatesComponent {
   templates: ItemTemplate[];
+  templatesToShow: ItemTemplate[];
   baseUrl = environment.spaUrl;
 
   totalItems: number;
   currentPage: number;
+  lower: number;
+  upper: number;
 
   constructor(private templateService: ItemTemplateService) {
     this.loadTemplates();
   }
 
-  setPage(pageNo: number): void {
-    this.currentPage = pageNo;
-  }
-
   pageChanged(event: any): void {
-    console.log('Page changed to: ' + event.page);
-    console.log('Number items per page: ' + event.itemsPerPage);
+    this.lower = event.itemsPerPage * (event.page - 1);
+    this.upper = (event.itemsPerPage * event.page) - 1;
+    console.log('Lower: ' + this.lower);
+    console.log('Upper: ' + this.upper);
+    this.templatesToShow = this.templates.slice(this.lower, this.upper);
   }
 
   loadTemplates() {
     this.templateService.getItemTemplates().subscribe(templates => {
       this.templates = templates;
       this.totalItems = this.templates.length;
+      this.templatesToShow = this.templates.slice(0, 15);
     });
   }
 
