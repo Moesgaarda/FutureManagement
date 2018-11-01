aa9124677e08e228293ff78346b4fbcfc195c65a
Unknown <rasmusedu@gmail.com>
2018-11-01 15:41:39 +0100

    Deleted some unnecessary stuff

--- spa/src/app/views/no-access/no-access.module.ts
+++ spa/src/app/views/no-access/no-access.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { NoAccessComponent } from './no-access.component';
 import { NoAccessRoutingModule } from './no-access-routing.module';
 
 @NgModule({
   imports: [
-    FormsModule,
-    CommonModule,
     NoAccessRoutingModule,
   ],
   declarations: [
