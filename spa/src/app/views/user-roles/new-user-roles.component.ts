53a2689a0c870218a091eff319fdfce43f502b9f
Andreas Stenshøj <and8700and@gmail.com>
2018-11-08 21:39:23 +0100

    added some functionality

--- spa/src/app/views/user-roles/new-user-roles.component.ts
+++ spa/src/app/views/user-roles/new-user-roles.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertifyService } from '../../_services/alertify.service';
 
 
 
@@ -8,20 +9,25 @@ import { Component, OnInit } from '@angular/core';
 
 export class NewUserRolesComponent implements OnInit {
 
+  constructor(private alertify: AlertifyService) {}
+
   userRoleTest: { id: number, name: string, hasRole: boolean, test: { id: number, name: string, hasRole: boolean }[] }[] = [
     {
-      'id': 0, 'name': 'Available', hasRole: false, test: [
-        { id: 0, name: 'hey', hasRole: false }, { id: 1, name: 'hey', hasRole: false }, { id: 2, name: 'hey', hasRole: false }
+      'id': 0, 'name': 'Kunder', hasRole: false, test: [
+        { id: 0, name: 'Vis alle kunder', hasRole: false }, { id: 1, name: 'Opret kunde', hasRole: false },
+        { id: 2, name: 'Rediger kunde', hasRole: false }
       ],
     },
     {
-      'id': 0, 'name': 'Ready', hasRole: false, test: [
-        { id: 0, name: 'hey', hasRole: false }, { id: 1, name: 'hey', hasRole: false }, { id: 2, name: 'hey', hasRole: false }
+      'id': 1, 'name': 'Projekter', hasRole: false, test: [
+        { id: 0, name: 'Vis alle projekter', hasRole: false }, { id: 1, name: 'Opret projekt', hasRole: false },
+        { id: 2, name: 'Rediger projekt', hasRole: false }
       ],
     },
     {
-      'id': 0, 'name': 'Yo', hasRole: false, test: [
-        { id: 0, name: 'hey', hasRole: false }, { id: 1, name: 'hey', hasRole: false }, { id: 2, name: 'hey', hasRole: false }
+      'id': 2, 'name': 'Bestillinger', hasRole: false, test: [
+        { id: 0, name: 'Vis alle bestillinger', hasRole: false }, { id: 1, name: 'Opret bestilling', hasRole: false },
+        { id: 2, name: 'Rediger bestilling', hasRole: false }
       ],
     }
   ];
@@ -39,5 +45,19 @@ export class NewUserRolesComponent implements OnInit {
       this.userRoleTest[id].hasRole = true;
       return true;
     }
+  }
+
+  changeHasSubRole(roleId: number, subRoleId: number) {
+    if (this.userRoleTest[roleId].test[subRoleId].hasRole === true) {
+      this.userRoleTest[roleId].test[subRoleId].hasRole = false;
+      return false;
+    } else {
+      this.userRoleTest[roleId].test[subRoleId].hasRole = true;
+      return true;
+    }
+  }
+
+  addRole() {
+    this.alertify.error('Kunne ikke tilføje rolle');
   }
 }
