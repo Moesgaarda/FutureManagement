51093dc4abf7da1156e09e9c9fe56cd0c84a09d8
Andreas Stenshøj <and8700and@gmail.com>
2018-11-09 11:12:49 +0100

    documentation and naming changes

--- spa/src/app/views/user-roles/new-user-roles.component.ts
+++ spa/src/app/views/user-roles/new-user-roles.component.ts
@@ -11,21 +11,22 @@ export class NewUserRolesComponent implements OnInit {
 
   constructor(private alertify: AlertifyService) {}
 
-  userRoleTest: { id: number, name: string, hasRole: boolean, test: { id: number, name: string, hasRole: boolean }[] }[] = [
+  // dette objekt er kun til test af UI. Skal ændres til en model når backend for userroles virker
+  userRoles: { id: number, name: string, hasRole: boolean, subRoles: { id: number, name: string, hasRole: boolean }[] }[] = [
     {
-      'id': 0, 'name': 'Kunder', hasRole: false, test: [
+      'id': 0, 'name': 'Kunder', hasRole: false, subRoles: [
         { id: 0, name: 'Vis alle kunder', hasRole: false }, { id: 1, name: 'Opret kunde', hasRole: false },
         { id: 2, name: 'Rediger kunde', hasRole: false }
       ],
     },
     {
-      'id': 1, 'name': 'Projekter', hasRole: false, test: [
+      'id': 1, 'name': 'Projekter', hasRole: false, subRoles: [
         { id: 0, name: 'Vis alle projekter', hasRole: false }, { id: 1, name: 'Opret projekt', hasRole: false },
         { id: 2, name: 'Rediger projekt', hasRole: false }
       ],
     },
     {
-      'id': 2, 'name': 'Bestillinger', hasRole: false, test: [
+      'id': 2, 'name': 'Bestillinger', hasRole: false, subRoles: [
         { id: 0, name: 'Vis alle bestillinger', hasRole: false }, { id: 1, name: 'Opret bestilling', hasRole: false },
         { id: 2, name: 'Rediger bestilling', hasRole: false }
       ],
@@ -36,28 +37,26 @@ export class NewUserRolesComponent implements OnInit {
 
   }
 
-
+  // Ændrer på bool værdien af en rolle når der ændres på en switch
   changeHasRole(id: number) {
-    if (this.userRoleTest[id].hasRole === true) {
-      this.userRoleTest[id].hasRole = false;
-      return false;
+    if (this.userRoles[id].hasRole === true) {
+      this.userRoles[id].hasRole = false;
     } else {
-      this.userRoleTest[id].hasRole = true;
-      return true;
+      this.userRoles[id].hasRole = true;
     }
   }
 
+  // Ændrer på bool værdien af en subrolle når der ændres på en switch
   changeHasSubRole(roleId: number, subRoleId: number) {
-    if (this.userRoleTest[roleId].test[subRoleId].hasRole === true) {
-      this.userRoleTest[roleId].test[subRoleId].hasRole = false;
-      return false;
+    if (this.userRoles[roleId].subRoles[subRoleId].hasRole === true) {
+      this.userRoles[roleId].subRoles[subRoleId].hasRole = false;
     } else {
-      this.userRoleTest[roleId].test[subRoleId].hasRole = true;
-      return true;
+      this.userRoles[roleId].subRoles[subRoleId].hasRole = true;
     }
   }
 
   addRole() {
+    // Skal implementeres når backend virker
     this.alertify.error('Kunne ikke tilføje rolle');
   }
 }
