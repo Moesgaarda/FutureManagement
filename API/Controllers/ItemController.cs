ba37df11e4bada7d6b21fec80e694eeb597adaeb
Daniel Moesgaard Andersen <moesgaarda@gmail.com>
2018-11-15 21:06:57 +0100

    Update API/Controllers/ItemController.cs

--- API/Controllers/ItemController.cs
+++ API/Controllers/ItemController.cs
@@ -11,7 +11,6 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
-    [AllowAnonymous]
     [Route("api/[controller]")]
     public class ItemController : Controller
     {
