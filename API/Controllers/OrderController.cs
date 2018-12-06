b57d83cc6d5276b84833b699e9124166d4fd619c
Daniel Moesgaard Andersen <moesgaarda@gmail.com>
2018-12-06 08:36:26 +0100

    Fixed Andreas comments

--- API/Controllers/OrderController.cs
+++ API/Controllers/OrderController.cs
@@ -11,7 +11,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers{
 
-    [AllowAnonymous]
     [Route("api/[controller]")]
     public class OrderController : Controller{
         private readonly DataContext _context;
--- spa/src/app/views/orders/orders-routing.module.ts
+++ spa/src/app/views/orders/orders-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     path: '',
     data: {
       title: 'Bestillinger',
-      roles: ['ItemTempaltes_View']
+      roles: ['ItemTemplates_View']
     },
     canActivate: [AuthGuard],
     children: [
