9ffc0f44d80c5732e30e8b8422a13dc9538b3cae
Andreas Stenshøj <and8700and@gmail.com>
2018-12-20 10:42:21 +0100

    removed some stuff from view page

--- spa/src/app/views/logs/log.module.ts
+++ spa/src/app/views/logs/log.module.ts
@@ -5,22 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { LogRoutingModule } from './log-routing.module';
 import { CommonModule } from '@angular/common';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { PaginationModule } from 'ngx-bootstrap/pagination';
-import { Ng2TableModule } from 'ng2-table';
-import { TechtableComponent } from '../../_components/techtable/techtable.component';
 
 @NgModule({
   imports: [
     LogRoutingModule,
-    PaginationModule.forRoot(),
     FormsModule,
     CommonModule,
     NgSelectModule,
-    Ng2TableModule
   ],
   declarations: [
     ViewLogComponent,
-    TechtableComponent
    ]
 })
 export class LogModule { }
