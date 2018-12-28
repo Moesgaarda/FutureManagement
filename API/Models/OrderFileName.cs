ad0ee46e986f7dffa7f7d44076f9fee93022665b
Unknown <jenspeturtrondarson@gmail.com>
2018-12-28 15:44:23 +0000

    Added missing Key tags

--- API/Models/OrderFileName.cs
+++ API/Models/OrderFileName.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Models
 {
     public class OrderFileName : IFileName
     {
+        [Key]
         public int Id { get; set; }
         public Order Order { get; set; }
         public string FileName { get; set; }
