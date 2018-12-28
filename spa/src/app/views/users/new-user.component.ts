013c28d168b4ddedd98e04ff0aa2165cc369ea5e
Frederik <frederikhtxq12@gmail.com>
2018-12-28 19:56:22 +0100

    added functionality, fixed user view aswell

--- spa/src/app/views/users/new-user.component.ts
+++ spa/src/app/views/users/new-user.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { AuthService } from '../../_services/auth.service';
+import { UserForRegister } from '../../_models/UserForRegister';
+import { AlertifyService } from '../../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './new-user.component.html'
@@ -7,8 +11,29 @@ import { environment } from '../../../environments/environment';
 
 export class NewUserComponent {
   baseUrl = environment.spaUrl;
+  user: UserForRegister = {} as UserForRegister;
+  passwordConfirm: string;
+  isValid = true;
 
+  constructor(private authService: AuthService, private alertify: AlertifyService, private router: Router) {
+  }
+  createUser() {
+    if (this.user.name !== undefined
+      && this.user.surname !== undefined
+      && this.user.username !== undefined
+      && this.user.password !== undefined
+      && this.user.password === this.passwordConfirm) {
+      this.authService.register(this.user).subscribe(() => {
+        this.alertify.success('Bruger var oprettet');
+        this.router.navigate(['users/view/']);
+      }, error => {
+        this.alertify.error('Kunne ikke tilføje bruger');
+      });
+    } else {
+      this.alertify.error('Brugeren var ikke valid');
+    }
+  }
   goToUserTable() {
-    location.href = this.baseUrl + '/users/view/';
+    this.router.navigate(['/users/view/']);
   }
 }
--- spa/src/app/views/users/view-users.component.ts
+++ spa/src/app/views/users/view-users.component.ts
@@ -13,10 +13,9 @@ export class ViewUsersComponent {
   baseUrl = environment.spaUrl;
   public rows: Array<any> = [];
   public columns: Array<any> = [
-    {title: 'Brugernavn', name: 'username'},
     {title: 'Fornavn', name: 'name'},
     {title: 'Efternavn', name: 'surname'},
-    {title: 'Roller', name: 'email'} // TODO NYI
+    {title: 'Email', name: 'email'} // TODO NYI
 ];
   public page = 1;
   public itemsPerPage = 5;
@@ -96,8 +95,10 @@ export class ViewUsersComponent {
     filteredData.forEach((item: any) => {
       let flag = false;
       this.columns.forEach((column: any) => {
-        if ((item[column.name].toString().toLowerCase()).match(this.config.filtering.filterString.toLowerCase())) {
-          flag = true;
+        if (item[column.name] !== undefined){
+          if ((item[column.name].toString().toLowerCase()).match(this.config.filtering.filterString.toLowerCase())) {
+            flag = true;
+          }
         }
       });
       if (flag) {
