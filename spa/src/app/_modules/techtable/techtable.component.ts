26402053d92d44a44bb12d61280c8d5a356e8525
Daniel Moesgaard Andersen <moesgaarda@gmail.com>
2018-12-30 20:14:07 +0100

    View users -> Techtable (#152)

--- spa/src/app/_modules/techtable/techtable.component.ts
+++ spa/src/app/_modules/techtable/techtable.component.ts
@@ -8,6 +8,7 @@ import { Injector } from '@angular/core';
 import { OrderService } from '../../_services/order.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AlertifyService } from '../../_services/alertify.service';
+import { UserService } from '../../_services/user.service';
 
 @Component({
   selector: 'app-techtable',
@@ -49,6 +50,8 @@ export class TechtableComponent implements OnInit {
       this.tableService = <ItemTemplateService>this.injector.get(ItemTemplateService);
     } else if (this.serviceType === 'OrderService') {
       this.tableService = <OrderService>this.injector.get(OrderService);
+    } else if (this.serviceType === 'UserService') {
+      this.tableService = <UserService>this.injector.get(UserService);
     } else {
       console.log('Unexpected service name: ' + this.serviceType);
     }
@@ -199,8 +202,10 @@ export class TechtableComponent implements OnInit {
       location.href = this.baseUrl + 'itemTemplates/details/' + data.row.id;
     } else if (this.serviceType === 'OrderService') {
       location.href = this.baseUrl + 'orders/details/' + data.row.id;
+    } else if (this.serviceType === 'UserService') {
+      location.href = this.baseUrl + 'users/details/' + data.row.id;
     } else {
-      this.alertify.error('Unexpected service name: ' + this.serviceType);
+      console.log('Unexpected service name: ' + this.serviceType);
     }
   }
 
