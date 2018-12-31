694a4f8a0f7b844bb3515bbac5ac2561d46d0880
andpaacoke <and8700and@gmail.com>
2018-12-31 11:58:33 +0100

    Vis og rediger item (#159)
    * stuff
    
    * Open pdf fix undefined error
    
    * item detail og item edit
    
    * fixed some async stuff
    
    * change to display name instead of username

--- spa/src/app/views/items/details-item.component.ts
+++ spa/src/app/views/items/details-item.component.ts
@@ -1,73 +1,69 @@
 import { Component, OnInit } from '@angular/core';
 import { ItemService } from '../../_services/item.service';
 import { Item } from '../../_models/Item';
-import { Order } from '../../_models/Order';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from '../../../environments/environment';
+import { AlertifyService } from '../../_services/alertify.service';
+
 
 @Component({
   templateUrl: './details-item.component.html'
 })
 export class DetailsItemComponent implements OnInit {
-  placementDisabled: boolean;
-  nameDisabled: boolean;
-  templateDisabled: boolean;
-  amountDisabled: boolean;
   item: Item;
   idForOrderDetail: number;
   baseUrl = environment.spaUrl;
+  itemLoaded: boolean;
+  partsLoaded: boolean;
+  parts: Item[] = [] as Item[];
 
-  constructor(
-    private itemService: ItemService,
-    private route: ActivatedRoute
+
+  constructor(private itemService: ItemService, private route: ActivatedRoute, private router: Router,
+    private alertify: AlertifyService
   ) {}
 
   // tslint:disable-next-line:use-life-cycle-interface
   ngOnInit() {
     this.loadItem();
-    this.templateDisabled = true;
-    this.placementDisabled = true;
-    this.amountDisabled = true;
   }
 
   async loadItem() {
     await this.itemService
       .getItem(+this.route.snapshot.params['id'])
-      .subscribe((item: Item) => {
-        this.item = item;
-        this.idForOrderDetail = item.order.purchaseNumber;
-        console.log(this.item);
+      .subscribe((itemToLoad: Item) => {
+        this.item = itemToLoad;
+        this.loadParts();
+      }, error => {
+        this.alertify.error('Kunne ikke hente info om genstanden');
+      }, () => {
+        this.itemLoaded = true;
       });
   }
 
-  enableTemplate() {
-    if (this.templateDisabled) {
-      this.templateDisabled = false;
-    } else {
-      // indsæt i db
-      this.templateDisabled = true;
-    }
+  async loadParts() {
+    this.item.parts.forEach(part => {
+      this.itemService.getItem(part.partId).subscribe(item => {
+        this.parts.push(item);
+      }, error => {
+        this.alertify.error('Kunne ikke hente info om genstanden');
+      }, () => {
+        if (this.parts.length === this.item.parts.length) {
+          this.partsLoaded = true;
+        }
+      });
+    });
   }
 
-  enablePlacement() {
-    if (this.placementDisabled) {
-      this.placementDisabled = false;
-    } else {
-      // indsæt i db
-      this.placementDisabled = true;
-    }
+  goToOrder() {
+    this.router.navigate(['orders/details/' + this.idForOrderDetail]);
   }
 
-  enableAmount() {
-    if (this.amountDisabled) {
-      this.amountDisabled = false;
-    } else {
-      // indsæt i db
-      this.amountDisabled = true;
-    }
+
+  goToEditPage() {
+    this.router.navigate(['items/edit/' + this.item.id]);
   }
 
-  routeToOrderDetail() {
-    location.href = this.baseUrl + 'orders/details/:idForOrderDetail';
+  goToTemplate() {
+    this.router.navigate(['itemTemplates/details/' + this.item.template.id]);
   }
 }
