e0a4475e48d10a40534ee73b4592f9750dd5331e
Unknown <jenspeturtrondarson@gmail.com>
2019-01-19 21:39:15 +0100

    fixed routing problem and added if statements in html to exlcude empty

--- spa/src/app/views/itemTemplates/details-itemTemplate.component.ts
+++ spa/src/app/views/itemTemplates/details-itemTemplate.component.ts
@@ -1,10 +1,10 @@
 import { environment } from '../../../environments/environment';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ItemTemplateService } from '../../_services/itemTemplate.service';
 import { ItemTemplate } from '../../_models/ItemTemplate';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 import { ItemPropertyName } from '../../_models/ItemPropertyName';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { ItemTemplatePart } from '../../_models/ItemTemplatePart';
 import { DetailFile } from '../../_models/DetailFile';
 import { FileUploadService } from '../../_services/fileUpload.service';
@@ -14,12 +14,13 @@ import { AuthService } from '../../_services/auth.service';
 @Component({
   templateUrl: './details-itemTemplate.component.html'
 })
-export class DetailsItemTemplateComponent implements OnInit {
+export class DetailsItemTemplateComponent implements OnInit, OnDestroy {
   isRevisioned = false;
   isDataAvailable = false;
   itemTemplate: ItemTemplate;
   files: DetailFile[];
   fileService: FileUploadService;
+  subscription: Subscription;
 
   constructor(
     private templateService: ItemTemplateService,
@@ -32,18 +33,24 @@ export class DetailsItemTemplateComponent implements OnInit {
   }
 
   async ngOnInit() {
-    await this.loadTemplateOnInIt();
+    this.subscription = this.route.params.subscribe(
+      (params: Params) => {
+        this.loadTemplateOnInIt(params);
+      }
+    );
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
-  async loadTemplateOnInIt() {
-    await this.templateService.getItemTemplateAsync(+this.route.snapshot.params['id'])
+  async loadTemplateOnInIt(params: Params) {
+    await this.templateService.getItemTemplateAsync(params['id'])
       .then(itemTemplate => {
         this.itemTemplate = itemTemplate;
         this.isDataAvailable = true;
         if (itemTemplate.revisionedFrom != null) {
           this.isRevisioned = true;
         }
-        console.log(this.itemTemplate);
     });
   }
 
@@ -55,6 +62,6 @@ export class DetailsItemTemplateComponent implements OnInit {
   }
 
   goToRevisionTemplate(){
-    this.router.navigateByUrl('itemTemplates/revise/' + this.itemTemplate.id);
+    this.router.navigate(['itemTemplates/revise/' + this.itemTemplate.id]);
   }
 }
