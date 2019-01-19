a386d79f0c67eeeaeb356c5aa44f29de535e1532
Thomas Lundsgaard Hansen <thomaslhansen@live.dk>
2019-01-19 09:56:29 +0100

    Updated to new OderStatus

--- API/Data/OrderRepository.cs
+++ API/Data/OrderRepository.cs
@@ -15,32 +15,39 @@ namespace API.Data
     {
         private readonly DataContext _context;
         private readonly ItemRepository _itemRepo;
-        public OrderRepository(DataContext context){
+        public OrderRepository(DataContext context)
+        {
             _context = context;
             _itemRepo = new ItemRepository(context);
         }
         public async Task<bool> AddOrder(Order order)
         {
-            if(order.OrderedBy != null){
+            if (order.OrderedBy != null)
+            {
                 order.OrderedBy = await _context.Users.FirstOrDefaultAsync(x => x.Id == order.OrderedBy.Id);
-            }else{
+            }
+            else
+            {
                 order.OrderedBy = await _context.Users.FirstOrDefaultAsync(x => x.Id == 1);
-            }            
-            foreach(Item product in order.Products){
+            }
+            foreach (Item product in order.Products)
+            {
                 product.Template = await _context.ItemTemplates.FirstOrDefaultAsync(x => x.Id == product.Template.Id);
-                foreach(ItemPropertyDescription property in product.Properties){
+                foreach (ItemPropertyDescription property in product.Properties)
+                {
                     property.PropertyName = await _context.ItemPropertyNames.FirstOrDefaultAsync(x => x.Id == property.PropertyName.Id);
                 }
-                
+
             }
-            order.UnitType = await _context.UnitTypes.FirstOrDefaultAsync( x => x.Name == order.UnitType.Name);
-            
-            foreach(var file in order.Files){
+            order.UnitType = await _context.UnitTypes.FirstOrDefaultAsync(x => x.Name == order.UnitType.Name);
+
+            foreach (var file in order.Files)
+            {
                 file.FileData = await _context.FileData.FirstOrDefaultAsync(x => x.Id == file.FileData.Id);
             }
 
             await _context.Orders.AddAsync(order);
-            
+
             int result = await _context.SaveChangesAsync();
 
             return result > 0;
@@ -64,19 +71,17 @@ namespace API.Data
         public async Task<List<Order>> GetAllOrders()
         {
             return await _context.Orders
-                .Include(x => x.Status)
-                .ToListAsync();
+            .ToListAsync();
         }
 
         public async Task<Order> GetOrder(int id)
         {
-            Order order =  await _context.Orders
+            Order order = await _context.Orders
             .Include(x => x.Products)
             .ThenInclude(x => x.Template)
             .Include(x => x.OrderedBy)
             .Include(x => x.Files)
             .ThenInclude(x => x.FileData)
-            .Include(x => x.Status)
             .Include(x => x.UnitType)
             .FirstOrDefaultAsync(x => x.Id == id);
 
