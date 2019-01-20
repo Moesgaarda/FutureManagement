a682df07467f4147eb0ff7e5629be738d75242f2
Thomas Lundsgaard Hansen <thomaslhansen@live.dk>
2019-01-20 22:50:30 +0100

    Almost got it to work

--- spa/src/app/views/orders/edit-order.component.ts
+++ spa/src/app/views/orders/edit-order.component.ts
@@ -3,6 +3,7 @@ import { OrderService } from '../../_services/order.service';
 import { AlertifyService } from '../../_services/alertify.service';
 import { Order } from '../../_models/Order';
 import { ActivatedRoute, Router } from '@angular/router';
+import { OrderStatusEnum } from '../../_enums/OrderStatusEnum.enum'
 
 @Component({
     templateUrl: './edit-order.component.html'
@@ -11,28 +12,38 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class EditOrderComponent implements OnInit {
     order: Order;
     isDataAvailable: boolean;
+    orderStatus: string;
+    orderStatusEnum = OrderStatusEnum;
+    keys: string[];
 
     constructor(private orderService: OrderService, private alertify: AlertifyService, private route: ActivatedRoute,
         private router: Router) { }
 
     async ngOnInit() {
         await this.loadOrderOnInIt();
+        this.keys = Object.keys(this.orderStatusEnum).filter(f => !isNaN(Number(f)));
     }
 
     async loadOrderOnInIt() {
         await this.orderService.getOrder(+this.route.snapshot.params['id'])
             .then(order => {
                 this.order = order;
+                this.orderStatus = OrderStatusEnum[order.status].toString();
                 this.isDataAvailable = true;
             });
     }
 
     updateStatus() {
-        this.orderService.statusUpdateOrder(this.order).subscribe(order => { }, error => {
-            this.alertify.error('Kunne ikke gennemføre ændringerne');
-        }, () => {
-            this.alertify.success('Status opdateret');
-            this.router.navigate(['order/details/' + this.order.id]);
-        });
+        this.order.status = this.orderStatusEnum[this.orderStatus];
+        console.log(this.order);
+        this.orderService.statusUpdateOrder(this.order).subscribe(
+            order => {
+                this.alertify.success('Status opdateret');
+            },
+            error => {
+                this.alertify.error('Kunne ikke gennemføre ændringerne');
+            }, () => {
+                this.router.navigate(['order/details/' + this.order.id]);
+            });
     }
 }
